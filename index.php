65d970fd7822cf34d787cd0e6229481d0ae21896
DaRelle Rollins <darelle.rollins@gmail.com>
2015-09-18 11:29:48 -0400

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+date_default_timezone_set('America/Kentucky/Louisville');
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
+
 echo 'Hello, World!';
